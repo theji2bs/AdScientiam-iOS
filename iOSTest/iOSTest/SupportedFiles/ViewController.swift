358a77341e23ac9dd7d020096908f3bdbc967b62
Elhadad MALIDI <malidi.elhadad@gmail.com>
2021-01-18 08:39:34 +0100

    init project

---
+++ iOSTest/iOSTest/SupportedFiles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTest
+//
+//  Created by Elhadad MALIDI on 18/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
